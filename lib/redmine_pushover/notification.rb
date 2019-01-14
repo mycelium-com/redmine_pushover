9ec887ab6bb01cc1ce522925adce01c42789c8c0
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2019-01-14 14:49:32 +0300

    Adds notification priority parsing

--- lib/redmine_pushover/notification.rb
+++ lib/redmine_pushover/notification.rb
@@ -3,8 +3,8 @@ module RedminePushover
 
     def initialize(mail)
       @recipients = []
-      @title = mail.subject
-      @message = get_message(mail)
+      @title      = mail.subject
+      @message    = get_message(mail)
     end
 
     def get_message(mail)
@@ -24,6 +24,7 @@ module RedminePushover
         message: @message,
         title:   @title
       }
+      message.merge! get_priority(@message)
       t = Thread.new do
         @recipients.each do |key|
           message[:user] = key
@@ -34,5 +35,25 @@ module RedminePushover
       @recipients.count
     end
 
+    def get_priority(text)
+      priority =
+        if text.include?('Priority: Immediate')
+          2
+        elsif text.include?('Priority: Urgent')
+          1
+        end
+      if priority == 2
+        {
+          priority: priority,
+          retry:    240,
+          expire:   10800
+        }
+      else
+        { priority: priority }
+      end
+    rescue => ex
+      Rails.logger.warn ex
+      {}
+    end
   end
 end
